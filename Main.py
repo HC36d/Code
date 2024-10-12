d282f1723cbdc19e64ff656f40e97dc2f508fbe5
HC36d <harshitaschill@gmail.com>
2024-10-12 21:15:40 +0530

    Main.py

---
+++ Main.py
@@ -0,0 +1,16 @@
+import discord
+
+intents = discord.Intents.default()
+intents.typing = False
+intents.presences = False
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user}')
+
+client.run('YOUR_BOT_TOKEN')
+
+
+
